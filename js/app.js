5b3866bc693c58ab0ea38ee929152b956a7340d8
michael-ahdi-aziz <michaelahdi6@gmail.com>
2021-08-16 03:05:43 +0300

    using forEach method to iterate through each section

--- js/app.js
+++ js/app.js
@@ -1,2 +1,6 @@
 // selecting all section tags in one variable
 let sections = document.querySelectorAll("section");
+//applying for each loop to iterate through each section 
+//
+sections.forEach(addNavigate);
+//through a function i will add link in the nav bar to each section 
