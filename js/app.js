46d0b3b470f0ae9f9e0d07a01db4e8761403f0cb
michael-ahdi-aziz <michaelahdi6@gmail.com>
2021-08-16 03:11:46 +0300

    making the nav menu as unordered list

--- js/app.js
+++ js/app.js
@@ -4,3 +4,11 @@ let sections = document.querySelectorAll("section");
 //
 sections.forEach(addNavigate);
 //through a function i will add link in the nav bar to each section 
+function addNavigate(elem){
+      /*adding the navigation menu as unordered list
+      *selecting ul tag to add to it the li 
+      linking each li tag to each section tag*/
+      let parentul = document.getElementById("navbar__list") ;
+      let childli = document.createElement("li");
+      parentul.appendChild(childli);
+}
